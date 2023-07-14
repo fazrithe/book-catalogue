26a05958607c3683509d0bb2cdf9b4f579a47d90
fazrithe <fazrithe@gmail.com>
2023-07-14 14:38:56 +0700

    update btn size

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -49,9 +49,9 @@
             },
             btnColor:'rgb(255,120,60)',
             sideBtnColor:'rgb(255,120,60)',
-            sideBtnSize:60,
+            sideBtnSize:30,
             sideBtnBackground:"rgba(0,0,0,.7)",
-            sideBtnRadius:60,
+            sideBtnRadius:30,
             btnSound:{vAlign:"top", hAlign:"left"},
             btnAutoplay:{vAlign:"top", hAlign:"left"},
             // SHARING
