37929218861417359c255c549a928082c2af94fe
fazrithe <fazrithe@gmail.com>
2023-07-14 14:24:41 +0700

    update
    update

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -3,7 +3,7 @@
 
   <head>
   <link rel="shortcut icon" href="">
-  <title>View PDF</title>
+  <title>Catalogue Tian Liong</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
